05a83f0e1490fa38525f08a74bbc17877242c273
emeoliveira <emeoliveira@uolinc.com>
2024-06-30 22:02:53 -0300

    simulação de banco e teste de create

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,22 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []  # simulação de banco
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Here we go again'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_com_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_com_id)
+
+    return user_com_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,25 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Here we go again'}
+
+
+def test_create_user():
+    client = TestClient(app)
+    response = client.post(  # Envio do user no schema
+        '/users/',
+        json={
+            'username': 'emerson',
+            'password': 'secret',
+            'email': 'emerson@example.com',
+        },
+    )
+
+    # Voltou o status code correto?
+    assert response.status_code == HTTPStatus.CREATED
+
+    # Validar UserPublic
+    assert response.json() == {
+        'username': 'emerson',
+        'id': 1,
+        'email': 'emerson@example.com',
+    }
